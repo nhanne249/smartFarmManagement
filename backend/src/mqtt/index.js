33ff7affe610b6eb0b26a1495bc2d9936c24db96
Van Phu <machvanphu1999@gmail.com>
2025-03-23 10:00:36 +0700

    test

--- backend/src/mqtt/index.js
+++ backend/src/mqtt/index.js
@@ -1,9 +1,33 @@
+require('dotenv').config();
 const mqtt = require('mqtt');
 const DayReport = require("../model/day");
 const day = require('../model/day');
 const send_Port = mqtt.connect('mqtt://localhost');
 const listen_Port = mqtt.connect('mqtt://localhost');
 // Khi gửi được tin nhắn tới IoT
+
+const ADAFRUIT_IO_USERNAME = process.env.ADAFRUIT_IO_USERNAME
+const ADAFRUIT_IO_KEY = process.env.ADAFRUIT_IO_KEY
+const bbc_temp = process.env.BBC_TEMP
+const bbc_humi = process.env.BBC_HUMI
+const MQTT_BROKER = `mqtts://${ADAFRUIT_IO_USERNAME}:${ADAFRUIT_IO_KEY}@io.adafruit.com`;
+const client = mqtt.connect(MQTT_BROKER);
+
+client.on('connect', () => {
+    client.subscribe(bbc_temp)
+    client.subscribe(bbc_humi)
+})
+
+client.on('message', (topic, message) => {
+    data = JSON.parse(message)
+    if (topic == bbc_temp) {
+        console.log(`tu feed nhiet do: ${data}`)
+        // client.publish(bbc_led, JSON.stringify(1))
+    } else if (topic == bbc_humi) {
+        console.log(`tu feed do am: ${data}`)
+        // client.publish(bbc_led, JSON.stringify(0))
+    }
+})
 
 function listen() {
     listen_Port.on('connect', () => {
