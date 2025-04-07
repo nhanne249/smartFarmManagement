7b2fc0e99b0d5a0505ac7222af327f3dcae6f769
Van Phu <machvanphu1999@gmail.com>
2025-04-07 13:19:22 +0700

    fix

--- backend/src/controller/sendController.js
+++ backend/src/controller/sendController.js
@@ -2,15 +2,23 @@ require('dotenv').config()
 const DayReport = require("../model/day")
 const client = require("../mqtt/index")
 
-const bbc_pump = process.env.BBC_FAN
-const BBC_PUMP = process.env.BBC_TEMP
+const bbc_fan = process.env.BBC_FAN
+const bbc_pump = process.env.BBC_PUMP
 
 const send = {
     api_fan_send: async (req, res) => {
-        data = JSON.stringify(parseInt(req.query.data))
+        data = 0
+        if (req.query.data == "true") {
+            data = 1
+        }
+        data = JSON.stringify(parseInt(data))
         client.publish(bbc_fan, data)
     },
     api_pmup_send: async (req, res) => {
+        data = 0
+        if (req.query.data == "true") {
+            data = 1
+        }
         data = JSON.stringify(parseInt(req.query.data))
         client.publish(bbc_pump, data)
     }
